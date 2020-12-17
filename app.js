b3723fc0f401af4b749f6335d8c360a86a4bc19b
Vellyus <noisyboy026@gmail.com>
2020-12-17 12:10:03 +0100

    first commit

--- app.js
+++ app.js
@@ -1,33 +1,19 @@
-/*
-
-const grid_size = 8
-function RandPoly()
+function angle(cx, cy, ex, ey)
 {
-  // let points = Math.round(Math.random() *5 + 3)
-  let points = 4
-  let poly = new Array(points)
-  // console.log(points) // print
-  let x = 0
-  let y = 0
-
-  for (let i = 0; i < points - 1; i++)
-  {
-    x = Math.round(Math.random() * 16 - 8)
-    y = Math.round(Math.random() * 16 - 8)
-    vector2 = [x, y]
-    poly.push(vector2)
-    // Print
-    console.log(poly.length)
-    for (let i = 0; i < poly.length; i++)
-    {
-      console.log(poly[i])
-    }
-  }
+  var dy = ey - cy;
+  var dx = ex - cx;
+  var theta = Math.atan2(dy, dx); // range (-PI, PI]
+  theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+  //if (theta < 0) theta = 360 + theta; // range [0, 360)
+  return theta;
 }
 
-RandPoly()
-
-*/
+function angle360(cx, cy, ex, ey)
+{
+  var theta = angle(cx, cy, ex, ey); // range (-180, 180]
+  if (theta < 0) theta = 360 + theta; // range [0, 360)
+  return theta;
+}
 
 const canvas = document.createElement("canvas")
 document.querySelector("body").appendChild(canvas)
@@ -46,108 +32,47 @@ function getRandomPoints(number)
 
   for (let i = 0; i < number; i++)
   {
+
     let point = {
       x: Math.floor(Math.random() * canvas.width),
       y: Math.floor(Math.random() * canvas.width)
     }
     points.push(point)
   }
-  return points
-}
 
-// returns true if the line from (a,b)->(c,d) intersects with (p,q)->(r,s)
-function intersects(a, b, c, d, p, q, r, s)
-{
-  var det, gamma, lambda;
-  det = (c - a) * (s - q) - (r - p) * (d - b);
-  if (det === 0)
-  {
-    return false;
-  } else
-  {
-    lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
-    gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
-    return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
-  }
+  return points.sort((a, b) => a.x - b.x)
 }
-
 
 function createRandomPoly(edges)
 {
   let points = getRandomPoints(edges)
-  let lines = []
-  let linesIntersect = false
+  points[0] = { x: canvas.width / 2, y: canvas.height / 2 }
 
   for (let i = 0; i < points.length; i++)
   {
-    if (i != points.length - 1)
-    {
-      let line = {
-        a: points[i].x,
-        b: points[i].y,
-        c: points[i + 1].x,
-        d: points[i + 1].y
-      }
-      lines.push(line)
-    }
-    else
-    {
-      let line = {
-        a: points[i].x,
-        b: points[i].y,
-        c: points[0].x,
-        d: points[0].y
-      }
-      lines.push(line)
-    }
-
+    ctx.fillStyle = 'black'
+    ctx.beginPath()
+    ctx.moveTo(points[0].x, points[0].y)
+    ctx.lineTo(points[i].x, points[i].y)
+    ctx.stroke()
   }
 
-  for (let i = 0; i < lines.length; i++)
-  {
-    for (let j = 0; j < lines.length; j++)
-    {
-      if (i != j)
-      {
-        if (intersects(lines[i].a, lines[i].b, lines[i].c, lines[i].d, lines[j].a, lines[j].b, lines[j].c, lines[j].d))
-        {
-          linesIntersect = true
-        }
-      }
-    }
-  }
+  points = points.sort((a, b) => angle360(canvas.width / 2, canvas.height / 2, a.x, a.y) - angle360(canvas.width / 2, canvas.height / 2, b.x, b.y))
+  points = points.filter(point => angle360(canvas.width / 2, canvas.height / 2, point.x, point.y) !== 0)
 
-  if (linesIntersect === true) points = createRandomPoly(edges)
-
-  console.log(linesIntersect)
-  return points
-}
-
-function render(points)
-{
-  ctx.fillStyle = 'black';
-  ctx.beginPath();
+  console.log(points)
 
   for (let i = 0; i < points.length; i++)
   {
-    if (i === 0) ctx.moveTo(points[i].x, points[i].y);
-    else ctx.lineTo(points[i].x, points[i].y)
-
+    ctx.fillStyle = 'black'
+    if (points[i + 1])
+    {
+      ctx.moveTo(points[i].x, points[i].y)
+      ctx.lineTo(points[i + 1].x, points[i + 1].y)
+    }
+    ctx.stroke()
   }
-  ctx.closePath();
-  ctx.fill();
 }
 
-render(createRandomPoly(4))
-
-
-// first point should be the one with the lowest x value
-// second should be with the second lowest x value
-// the next line should be with the lowest angle to that line
-
-
-
-
-
-
+createRandomPoly(4)
 
